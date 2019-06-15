789ddf9e9d704f538ac7126bbd0a3c7a00937575
rainweic <861514706@qq.com>
2019-06-15 12:00:58 +0800

    删除没用文件

--- src/heatmap-2.2.1/examples/google-earth.py
+++
@@ -1,7 +0,0 @@
-import heatmap
-import random
-
-hm = heatmap.Heatmap()
-pts = [(random.uniform(-77.012, -77.050), random.uniform(38.888, 38.910)) for x in range(100)]
-hm.heatmap(pts)
-hm.saveKML("wash-dc.kml")
